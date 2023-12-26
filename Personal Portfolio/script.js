6355b591c34b5a42e40995bf525458492372c4c9
EhsanRaza2238 <148682858+EhsanRaza2238@users.noreply.github.com>
2023-12-26 13:13:21 +0500

    Add files via upload

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // You can add code here to handle the form submission, e.g., sending an email or storing data.
+    
+    alert('Message sent successfully!');
+});
